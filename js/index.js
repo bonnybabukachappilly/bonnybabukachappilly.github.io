248b1dd35a7e1f5b2a9987d39376e896fea9b401
bonnybabukachappilly <bonnybabukachappilly@gmail.com>
2024-04-06 11:35:34 +0530

    basic version

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Robotics Engineer", "Electrical Engineer", "ROS Developer", "Python Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
